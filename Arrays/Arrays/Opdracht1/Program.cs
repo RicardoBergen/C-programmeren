d1f9532cd61531355c2b1b6f362537ce9bf53648
Ricardo <148498@student.horizoncollege.nl>
2021-09-06 14:16:19 +0200

    Opdracht1
    Ik heb opdracht1 gemaakt

---
+++ Arrays/Arrays/Opdracht1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Opdracht1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = { "Peter", "is", "de", "broer", "van", "Hans", "" };
+            // 1 voor 1 de worden laten zien op de zelfde lijn
+            foreach (string word in array)
+            {
+                Console.Write(word + " ");
+            }
+            Console.WriteLine("");
+            // De worden Peter en Hans met elkaar wisselen
+            array[6] = array[5];
+            array[5] = array[0];
+            array[0] = array[6];
+            array[6] = "";
+            foreach (string word in array)
+            {
+                Console.Write(word + " ");
+            }
+        }
+    }
+}
---
+++ Arrays/Arrays/Opdracht2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Opdracht2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rij = 1;
+            for (int colom = 0; colom < 3; colom++)
+            {
+                for (rij; rij < 3; rij++)
+                {
+                    Console.Write(rij + " ");
+                }
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
