1f2218890094ac1560751b07c4a9ef503dec9156
Ricardo <148498@student.horizoncollege.nl>
2021-09-10 09:02:02 +0200

    OOP opdracht 1
    OOP opdracht 1 gemaakt

---
+++ OOP/OOP/Opdracht1/Persoon.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Personen
+{
+    public class Persoon
+    {
+        public string Voornaam { get; set; }
+        public string Achternaam { get; set; }
+        public DateTime Geboortedatum { get; set; }
+        public string Geslacht { get; set; }
+        public int Lengte { get; set; }
+    }
+}
