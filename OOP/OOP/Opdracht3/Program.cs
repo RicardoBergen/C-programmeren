fd703cff53c1dbfb5b334a765b81e9680692073f
Ricardo <148498@student.horizoncollege.nl>
2021-09-29 09:04:22 +0200

    OOP opdracht twee

--- OOP/OOP/Opdracht3/Program.cs
+++ OOP/OOP/Opdracht3/Program.cs
@@ -12,18 +12,22 @@ namespace Opdracht3
             List<Persoon> personen = new List<Persoon>();
         }
         //private static List<Persoon> personen = new List<Persoon>();
-        private static List<Persoon> personen;
-    }
+        public static List<Persoon> personen;
 
-    private static void Invullen()
-    {
-        Persoon Persoon1 = new Persoon();
-        Persoon1.Voornaam = "Piet";
-        Persoon1.Achternaam = "Pieterson";
-        Persoon1.Geboortedatum = new DateTime(2018, 7, 24);
-        Persoon1.Geslacht = "M/M";
-        Persoon1.Lengte = 190;
-        personen.Add(Persoon1);
-    }
 
+        private static void Invullen()
+        {
+            Persoon Persoon1 = new Persoon();
+            Persoon1.Voornaam = "Piet";
+            Persoon1.Achternaam = "Pieterson";
+            Persoon1.Geboortedatum = new DateTime(2018, 7, 24);
+            Persoon1.Geslacht = "M/M";
+            Persoon1.Lengte = 190;
+            personen.Add(Persoon1);
+        }
+        private static void PersonenWeergeven()
+        {
+
+        }
+    }
 }
